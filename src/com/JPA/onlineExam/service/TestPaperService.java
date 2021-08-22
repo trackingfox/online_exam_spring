356e2d5581e19b084c270b3bb2f1c65174c0fbb9
trackingfox <sharifali1993@gmail.com>
2021-08-22 23:54:56 +0530

    Changed file Commit.

--- src/com/JPA/onlineExam/service/TestPaperService.java
+++ src/com/JPA/onlineExam/service/TestPaperService.java
@@ -3,6 +3,12 @@ package com.JPA.onlineExam.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -58,4 +64,46 @@ public class TestPaperService {
 		testPaperList.forEach(x -> repository.save(x));
 
 	}
+
+	@Test
+	public void generateTestPaper1() {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA_Online_Exam");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		List<Topic> topics = repository3.FetchTopics();
+		for (Topic obj : topics) {
+			//
+			System.out.println(obj);
+		}
+
+		for (int i = 1; i <= 4; i++) {
+			Query query = em.createQuery(
+					"FROM Question where Id>=FLOOR(RAND()*(25-10+1))+10 AND Id<FLOOR(RAND()*(50-10+1))+30 ");
+			List<Question> results = query.getResultList();
+
+//			for (Question obj : results) {
+//				System.out.println(obj.getQuestion() + "   " + obj.getChoice_1() + "   " + obj.getChoice_2() + "    "
+//						+ obj.getChoice_3() + "    " + obj.getChoice_4());
+//
+//			}
+
+			// int ran = (int) (Math.random() * (3 - 1 + 1) + 1);
+
+			TestPaper test1 = new TestPaper();
+			test1.setQuestionSet(results);
+			test1.setTestName("Full Stack JAVA");
+			test1.setTestLevel("I");
+			test1.setTopics(topics);
+			em.merge(test1);
+
+		}
+
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+
+	}
+
 }
