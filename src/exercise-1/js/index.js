f5fba2c1d7c5a7fd9ec9b7ee16b01dcad3a4c830
trackingfox <sharifali1993@gmail.com>
2021-10-29 23:48:28 +0530

    Changed file Commit.

--- src/exercise-1/js/index.js
+++ src/exercise-1/js/index.js
@@ -129,5 +129,24 @@ circle.draw();
 
 
 
+let num = Number("346")
+
+console.log(num, typeof num);
+
+let x
+console.log(x, typeof x);
+x = 8
+console.log(x, typeof x);
+x = x + ""
+console.log(x, typeof x);
+
+x = x - 2
+console.log(x, typeof x);
+
+x = !x
+console.log(x, typeof x);
+
+
+
 
 
